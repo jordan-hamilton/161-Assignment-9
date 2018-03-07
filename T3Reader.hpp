e9a6f1deaf86c22dee2c0a625bd76aea7be29f1b
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-03-07 07:12:49 -0700

    Added class template

--- T3Reader.hpp
+++ T3Reader.hpp
@@ -3,3 +3,8 @@
  ** Date: 03/06/2018
  ** Description: Specification file for the T3Reader class.
  *********************************************************************/
+
+class T3Reader {
+  private:
+  public:
+};
