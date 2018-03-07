c57807e71b9799991d6cb92ce7b266b2b8a49fbb
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-03-07 07:12:24 -0700

    Added constructor, makeMove, and print functions

--- Board.cpp
+++ Board.cpp
@@ -1,5 +1,41 @@
 /*********************************************************************
  ** Author: Jordan Hamilton
  ** Date: 03/06/2018
- ** Description: This implements a class
+ ** Description: This implements a class that represents a tic-tac-toe
+ ** board.
  *********************************************************************/
+
+#include <iostream>
+
+#include "Board.hpp"
+
+Board::Board() {
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      gameBoard[x][y] = 32;
+    }
+  }
+}
+
+bool Board::makeMove(int xCoordinate, int yCoordinate, char player) {
+  if (gameBoard[xCoordinate][yCoordinate] == 32) {
+    gameBoard[xCoordinate][yCoordinate] = player;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+State Board::gameState() {
+    State gameState = UNFINISHED;
+  }
+
+void Board::print() {
+  std::cout << " ___________ " << std::endl;
+  for (int y = 0; y < 3; y++) {
+    for (int x = 0; x < 3; x++) {
+      std::cout << "|_" << gameBoard[x][y] << "_";
+    }
+    std::cout << "|" << std::endl;
+  }
+}
--- Board.hpp
+++ Board.hpp
@@ -3,3 +3,18 @@
  ** Date: 03/06/2018
  ** Description: Specification file for the Board class.
  *********************************************************************/
+
+
+enum State { X_WON, O_WON, DRAW, UNFINISHED };
+
+class Board {
+
+  private:
+    char gameBoard[3][3];
+
+  public:
+    Board();
+    bool makeMove(int xCoordinate, int yCoordinate, char player);
+    State gameState();
+    void print();
+};
